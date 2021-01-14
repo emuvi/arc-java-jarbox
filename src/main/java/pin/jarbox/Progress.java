98423ab64af5e956cf9f952df56789c9f6353cd1
Everton Murilo Vieira <emuvs@pointel.com.br>
2021-01-14 20:04:05 -0300

    Change the model for log and interact with it

---
+++ src/main/java/pin/jarbox/Progress.java
@@ -0,0 +1,55 @@
+package pin.jarbox;
+
+public abstract class Progress {
+
+  private volatile boolean paused;
+  private volatile boolean stopped;
+
+  public Progress() {
+    paused = false;
+    stopped = false;
+  }
+
+  public synchronized void pause() {
+    paused = true;
+  }
+
+  public synchronized void resume() {
+    paused = false;
+  }
+
+  public synchronized void stop() {
+    stopped = true;
+  }
+
+  public synchronized boolean isPaused() {
+    return paused;
+  }
+
+  public synchronized boolean isStopped() {
+    return stopped;
+  }
+
+  public abstract void log(String message);
+
+  public void log(Exception error) {
+    log(Utils.getDescription(error));
+  }
+
+  public boolean waitIfPausedAndCheckStopped() throws Exception {
+    while (isPaused()) {
+      Thread.currentThread().sleep(10);
+    }
+    return isStopped();
+  }
+
+  public void waitIfPausedAndThrowIfStopped() throws Exception {
+    while (isPaused()) {
+      Thread.currentThread().sleep(10);
+    }
+    if (isStopped()) {
+      throw new Exception("Process was stopped.");
+    }
+  }
+
+}
